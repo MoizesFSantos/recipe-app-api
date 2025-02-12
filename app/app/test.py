4ea736cdc145a93ba742e05d63dbec3ab88e81ab
MoizesFSantos <moizesferreira66@hotmail.com>
2025-02-12 11:34:55 +0000

    Add PostgreSQL support and basic calculator functionality
    - Update Dockerfile to include PostgreSQL client and dependencies
    - Modify docker-compose.yml to add PostgreSQL service
    - Update requirements.txt to include psycopg2
    - Implement basic calculator functions (add, subtract) in calc.py
    - Add unit tests for calculator functions in test.py

---
+++ app/app/test.py
@@ -0,0 +1,18 @@
+
+
+from django.test import SimpleTestCase # type: ignore
+
+from app import calc
+
+class CalcTest(SimpleTestCase):
+
+    def test_add_numbers(self):
+        res = calc.add(3, 8)
+
+        self.assertEqual(res, 11)
+    def test_subtract_numbers(self):
+        res = calc.subtract(5, 11)
+
+        self.assertEqual(res, 6)
+
+    
